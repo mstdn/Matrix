85cb6785b671bfc35f8b8ac1215018f52118e817
stux <hello@mstdn.social>
2023-11-13 03:07:21 +0100

    Some text

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,11 +1,29 @@
+import { Float, Text } from "@react-three/drei"
 import { Matrix } from "./Matrix"
 
 export default function World()
 {
     return(<>
-        <Matrix 
-            rotation-y={ Math.PI * 0.95 }
-            position={ [0, - 1, 0] }
-        />
+        <group>
+            <Matrix 
+                rotation-y={ Math.PI * 0.95 }
+                position={ [0, - 1, 0] }
+            />
+            <Float>
+                <Text
+                    font="./assets/fonts/matrix.ttf"
+                    fontSize={ 0.5 }
+                >
+                    Matrix
+                </Text>
+                <Text
+                    position={ [0, - 0.3, 0] }
+                    font="./assets/fonts/matrix.ttf"
+                    fontSize={ 0.3 }
+                >
+                    tududuuu
+                </Text>
+            </Float>
+        </group>
     </>)
 }
